12540028bb31d6e89896cae4cd128c575de01b87
jaedench <jaedench@hawaii.edu>
2022-04-15 09:35:39 -1000

    hello world working

---
+++ cmake-build-debug/CMakeFiles/ee205_lab12a_fatCat.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ee205_lab12a_fatCat.dir/main.cpp.o"
+  "CMakeFiles/ee205_lab12a_fatCat.dir/main.cpp.o.d"
+  "ee205_lab12a_fatCat"
+  "ee205_lab12a_fatCat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ee205_lab12a_fatCat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ee205_lab12a_fatCat.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ee205_lab12a_fatCat.
+# This may be replaced when dependencies are built.
